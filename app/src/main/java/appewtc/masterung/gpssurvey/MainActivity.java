e3ee120567d4abb7923fe21548e78c2c4cbfc75f
masterUNG <phrombutr@gmail.com>
2016-03-28 10:54:38 +0700

    แก้ปัญหาDistance แว้วว

--- app/src/main/java/appewtc/masterung/gpssurvey/MainActivity.java
+++ app/src/main/java/appewtc/masterung/gpssurvey/MainActivity.java
@@ -132,7 +132,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         //Calculate Triangle
         double area = triangleArea(distance1, distance2, distance3);
-        Log.d("28March", "area = " + area);
+
+        //Test Distance
+        Log.d("28March", "point 1-2 ==> " + distance1);
 
 
     } // clickFinish
@@ -149,12 +151,23 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     //นี่คือ เมทอด ที่หาระยะ ระหว่างจุด
-    private double distance(double douX1, double douY1, double douX2, double douY2) {
+    private static double distance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515* 1.609344;
 
-        Double douDistance = Math.sqrt( Math.pow((douX1-douX2),2) + Math.pow((douY1-douY2),2) );
 
+        return (dist);
+    }
 
-        return douDistance;
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 
 
