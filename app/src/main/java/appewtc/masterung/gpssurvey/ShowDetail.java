45e1ac660a8d8c12f0b70f288e54acbf4c16f46f
masterUNG <phrombutr@gmail.com>
2016-04-18 22:06:02 +0700

    Show View Finish

--- app/src/main/java/appewtc/masterung/gpssurvey/ShowDetail.java
+++ app/src/main/java/appewtc/masterung/gpssurvey/ShowDetail.java
@@ -1,7 +1,8 @@
 package appewtc.masterung.gpssurvey;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,27 +14,44 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class ShowDetail extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private TextView nameTextView, dateTextView, areaTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_detail);
+
+        //Bind Widget
+        bindWidget();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map2);
         mapFragment.getMapAsync(this);
+
+        //Show View
+        showView();
+
+    }   // Main Method
+
+    private void bindWidget() {
+        nameTextView = (TextView) findViewById(R.id.textView18);
+        dateTextView = (TextView) findViewById(R.id.textView19);
+        areaTextView = (TextView) findViewById(R.id.textView20);
     }
 
+    private void showView() {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        String strName = getIntent().getStringExtra("Name");
+        String strDate = getIntent().getStringExtra("Date");
+        String strArea = getIntent().getStringExtra("Area");
+
+        nameTextView.setText(strName);
+        dateTextView.setText(strDate);
+        areaTextView.setText(strArea);
+    }   // showView
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -42,5 +60,6 @@ public class ShowDetail extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+    }   // onMap
+
+}   // Main Class
